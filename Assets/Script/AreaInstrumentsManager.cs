4ce5b9ea7f1f71143d45cd74600b282da3904dfa
IbRyth <3361654175@qq.com>
2025-09-13 17:14:03 +0800

    Add AreaInstrumentsManager.cs - 区域乐器管理器

---
+++ Assets/Script/AreaInstrumentsManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaInstrumentsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] InstrumentsInArea;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
